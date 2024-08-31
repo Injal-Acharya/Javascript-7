70e673040f261a6463c936e2bff49755281672f5
AI-3Tech <injalacharya1@gmail.com>
2024-08-31 17:04:53 +0545

    Walking the DOM

---
+++ Dom.js
@@ -0,0 +1,6 @@
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
+console.log(document.body.childNodes);
+let arr = Array.from(document.body.childNodes);
+console.log(arr[arr.length -1]);
+console.log(arr.length);
